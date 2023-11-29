357d350cf883861693a75845b5634f6fcab30103
HOME_Ivan <vanyuha3005@gmail.com>
2023-11-30 00:19:14 +0300

    Домашние задания по Java #3 module05.

--- module05/src/main/java/ru/sberbank/edu/TravelService.java
+++ module05/src/main/java/ru/sberbank/edu/TravelService.java
@@ -1,5 +1,4 @@
 package ru.sberbank.edu;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +7,6 @@ import java.util.List;
  */
 public class TravelService {
 
-    // do not change type
     private final List<CityInfo> cities = new ArrayList<>();
 
     /**
@@ -18,7 +16,10 @@ public class TravelService {
      * @throws IllegalArgumentException if city already exists
      */
     public void add(CityInfo cityInfo) {
-        // do something
+        if (cities.stream().filter(c->c.getName().equals(cityInfo.getName())).toList().size() != 0){
+            throw new IllegalArgumentException("if city already exists");
+        }
+        else {cities.add(cityInfo);}
     }
 
     /**
@@ -28,6 +29,21 @@ public class TravelService {
      * @throws IllegalArgumentException if city doesn't exist
      */
     public void remove(String cityName) {
+
+        if (cities.stream().filter(c->c.getName().equals(cityName)).toList().size() == 0){
+            throw new IllegalArgumentException("if city doesn't exist");}
+
+        else{cities.removeIf(c -> cityName.equalsIgnoreCase(c.getName()));}
+
+//        this.cities.stream().map((cityInfo) -> {
+//            if (cityInfo.getters().containsKey(cityName)) {
+//                cityInfo.getters().remove(cityName);
+//
+//            }else {
+//                throw new IllegalArgumentException("if city doesn't exist");
+//            }
+//            return this.cities;
+//        });
         // do something
     }
 
@@ -35,7 +51,7 @@ public class TravelService {
      * Get cities names.
      */
     public List<String> citiesNames() {
-        return null;
+        return cities.stream().map(CityInfo::getName).toList();
     }
 
     /**
@@ -47,8 +63,42 @@ public class TravelService {
      * @throws IllegalArgumentException if source or destination city doesn't exist.
      */
     public int getDistance(String srcCityName, String destCityName) {
-        return 0;
-    }
+
+        if (!this.citiesNames().contains(srcCityName) && !this.citiesNames().contains(destCityName)){
+            throw new IllegalArgumentException("if source or destination city doesn't exist.");}
+
+        // Радиус земли
+        final double EARTH_RADIUS = 6372795;
+
+        double srcLatitude = 0, srcLongitude = 0, destLatitude = 0, destLongitude = 0;
+
+        List<CityInfo> src = cities.stream().filter(CityInfo -> CityInfo.getName().equals(srcCityName)).toList();
+        srcLatitude = src.get(0).getters().get(srcCityName).getters()[0];
+        srcLongitude = src.get(0).getters().get(srcCityName).getters()[1];
+
+        List<CityInfo> dest = cities.stream().filter(CityInfo -> CityInfo.getName().equals(destCityName)).toList();
+        destLatitude = dest.get(0).getters().get(destCityName).getters()[0];
+        destLongitude = dest.get(0).getters().get(destCityName).getters()[1];
+
+        double cl1 = Math.cos(srcLatitude);
+        double cl2 = Math.cos(destLatitude);
+        double sl1 = Math.sin(srcLatitude);
+        double sl2 = Math.sin(destLatitude);
+        double delta = destLongitude - srcLongitude;
+        double cdelta = Math.cos(delta);
+        double sdelta = Math.sin(delta);
+
+        // вычисления длины большого круга
+        double y = Math.sqrt(Math.pow(cl2 * sdelta, 2) + Math.pow(cl1 * sl2 - sl1 * cl2 * cdelta, 2));
+        double x = sl1 * sl2 + cl1 * cl2 * cdelta;
+
+        double ad = Math.atan2(y, x);
+        double dist = ad * EARTH_RADIUS;
+
+        return (int)dist;
+
+
+}
 
     /**
      * Get all cities near current city in radius.
@@ -58,6 +108,8 @@ public class TravelService {
      * @throws IllegalArgumentException if city with cityName city doesn't exist.
      */
     public List<String> getCitiesNear(String cityName, int radius) {
-        return null;
+        if (!this.citiesNames().contains(cityName)){
+            throw new IllegalArgumentException("if city with cityName city doesn't exist.");}
+        return cities.stream().filter(f -> ((this.getDistance(cityName, f.getName()) <= radius) && (!f.getName().equals(cityName)))).map(CityInfo::getName).toList();
     }
 }
